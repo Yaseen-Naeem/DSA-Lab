bb1988b61d75f0e3c742e04e326bc8039a32a833
Yaseen Naeem <K250791@nu.edu.pk>
2026-09-09 09:33:13 +0500

    InsertionSort.cpp

--- In-Lab Task 3/InsertionSort.cpp
+++ In-Lab Task 3/InsertionSort.cpp
@@ -6,7 +6,6 @@ void insertionSort(int arr[], int n) {
         int key = arr[i];
         int j = i - 1;
 
-        // Shift elements that are greater than key to one position ahead
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
